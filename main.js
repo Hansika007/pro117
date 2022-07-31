870fe36dc966b57568e7bb6a4e4578ae96db8b23
Hansika007 <95028012+Hansika007@users.noreply.github.com>
2022-07-31 16:26:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+timer_counter = 0;
+score = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+
+array_1=["pen","paper","book","bottle"];
+
+random_no = Math.floor((Math.random()*array_1.length)+1);
+
+Element_of_array = array_1[random_no];
